e0cc2dc3e745d9339811ba99f1b9c03f6cce5afe
jobbob1231234 <jobbob1231234@gmail.com>
2025-01-08 09:39:41 +0800

    Site updated: 2025-01-08 09:39:40

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/01/07/hello-world/","2025/01/07/mytest/","2025/01/07/mytest1/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
